0081eecc392254a1ef4dd720d51470a0ea5903f8
Dimpozix <pleazzer073@gmail.com>
2026-05-18 22:11:28 +1000

    added header

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import ActivitiesSection from '../interactive/ActivitiesSection';
-import Gallery from './components/Gallery'; // Импортируем наш новый компонент
+import Gallery from './components/Gallery';
 import ReviewsSection from '../reviews/ReviewsSection';
 
 const Main: React.FC = () => {
     return (
-        <Box component="main" sx={{ flexGrow: 1 }}>
-            {/* Теперь здесь настоящая галерея */}
+        // Добавили paddingTop, равный примерной высоте хедера, чтобы галерея не пряталась под ним
+        <Box component="main" sx={{ flexGrow: 1, pt: '70px' }}>
+
             <Gallery />
-            <ActivitiesSection />
-            <ReviewsSection />
+
+            {/* Добавили id="activities" */}
+            <div id="activities">
+                <ActivitiesSection />
+            </div>
+
+            {/* Добавили id="reviews" */}
+            <div id="reviews">
+                <ReviewsSection />
+            </div>
+
         </Box>
     );
 };
