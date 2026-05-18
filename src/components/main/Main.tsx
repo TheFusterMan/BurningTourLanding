7c874d02686c373184985291790df5e1fa1d60b8
Валентин Зайнулин <zainulin.vs@dvfu.ru>
2026-05-18 21:08:18 +1000

    feat: add reviews section

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import ActivitiesSection from '../interactive/ActivitiesSection';
+import ReviewsSection from '../reviews/ReviewsSection';
 
 const Gallery: React.FC = () => <Box sx={{ height: 400, bgcolor: '#cfd8dc', display: 'flex', alignItems: 'center', justifyContent: 'center' }}><h2>Галерея (Слайдер)</h2></Box>;
-const Reviews: React.FC = () => <Box sx={{ height: 300, bgcolor: '#e0e0e0', display: 'flex', alignItems: 'center', justifyContent: 'center' }}><h2>Отзывы</h2></Box>;
 
 const Main: React.FC = () => {
     return (
         <Box component="main" sx={{ flexGrow: 1 }}>
             <Gallery />
             <ActivitiesSection />
-            <Reviews />
+            <ReviewsSection />
         </Box>
     );
 };
